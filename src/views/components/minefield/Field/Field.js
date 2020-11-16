a872b76d7c657f1f368d0b2dd54a2675f17de881
Felipe Almeida <felipe.silvalmeida@gmail.com>
2020-11-16 00:29:41 -0300

    Estilização básica dos inputs

--- src/views/components/minefield/Field/Field.js
+++ src/views/components/minefield/Field/Field.js
@@ -5,7 +5,8 @@ import Button from 'react-bootstrap/Button';
 export default function Field(props) {
     return (
         <div className="field">
-            <Button variant="outline-primary" onClick={() => props.onToggle()}
+            <Button variant={props.field.isToggled ? "outline-primary" : "primary"}
+                onClick={() => props.onToggle()}
                 disabled={props.disabled}>
                 {props.field.isToggled ? props.field.value : '-'}
             </Button>
